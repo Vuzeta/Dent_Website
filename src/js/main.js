5dc5f70be91817e7cd6b03b4008604e25ab523e4
Vuzeta <machniik95@gmail.com>
2019-11-04 12:53:46 +0100

    add price section, table showing

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,34 @@ import '../assets/doctor_640.jpg';
 import '../assets/doctor2_640.jpg';
 import '../assets/doctor3_640.jpg';
 
+//HAMBURGER
 const hamburgerElement = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
 
 hamburgerElement.addEventListener('click', () => {
-  hamburgerElement.classList.toggle('open');
-  navigation.classList.toggle('navigation--show');
+	hamburgerElement.classList.toggle('open');
+	navigation.classList.toggle('navigation--show');
+});
+
+//PRICE SHOW
+const priceItems = [...document.querySelectorAll('.price__wrapper-list-item')];
+priceItems.forEach(item => {
+	item.addEventListener('click', function(e) {
+		this.classList.toggle('price__wrapper-list-item--show');
+
+		const itemHeight = this.clientHeight;
+		const tableHeight = this.querySelector('.price__wrapper-table').clientHeight;
+		const icon = item.querySelector('.price__icon');
+		const headerHeight = item.querySelector('.price__wrapper-list-col').clientHeight;
+
+		// item.style.height = `${tableHeight + itemHeight}px`;
+
+		if (item.classList.contains('price__wrapper-list-item--show')) {
+			icon.style.transform = 'scale(-1)';
+			item.style.height = `${tableHeight + itemHeight}px`;
+		} else {
+			icon.style.transform = 'scale(1)';
+			item.style.height = `${headerHeight}px`;
+		}
+	});
 });
