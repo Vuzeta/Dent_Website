c63d96f03025d8c0682966fbcd6b2b2d9adb5c5f
Vuzeta <machniik95@gmail.com>
2019-11-06 12:32:44 +0100

    add alert after submit form

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import swal from 'sweetalert';
 import '../scss/main.scss';
 import '../assets/hero_640.mp4';
 import '../assets/doctor_640.jpg';
@@ -8,6 +9,7 @@ import '../assets/doctor3_640.jpg';
 const hamburgerElement = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
 const formInputs = [...document.querySelectorAll('.contact__form > *[data-name="form-element"]')];
+const form = document.querySelector('.contact__form');
 
 hamburgerElement.addEventListener('click', () => {
 	hamburgerElement.classList.toggle('open');
@@ -24,8 +26,6 @@ priceItems.forEach(item => {
 		const tableHeight = this.querySelector('.price__wrapper-table').clientHeight;
 		const icon = item.querySelector('.price__icon');
 		const headerHeight = item.querySelector('.price__wrapper-list-col').clientHeight;
-
-		// item.style.height = `${tableHeight + itemHeight}px`;
 
 		if (item.classList.contains('price__wrapper-list-item--show')) {
 			icon.style.transform = 'scale(-1)';
@@ -46,4 +46,16 @@ formInputs.forEach(input => {
 			e.target.placeholder = valueBefore;
 		});
 	});
+});
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+
+	swal({
+		text: 'Wiadomość wysłano',
+		buttons: false,
+		icon: 'success',
+		timer: 2000,
+	});
+	formInputs.forEach(input => (input.value = ''));
 });
