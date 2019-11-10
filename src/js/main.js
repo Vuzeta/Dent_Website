500d3d309ac5d7e59f068f3673ab1fc999513cb6
Vuzeta95 <machnio95@o2.pl>
2019-11-10 11:32:27 +0100

    refactoring code

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,6 @@ import '../assets/dental_960.mp4';
 import '../assets/dental_1280.mp4';
 import '../assets/dental_1920.mp4';
 
-//HAMBURGER
 const hamburgerElement = document.querySelector('.hamburger');
 const priceItems = [...document.querySelectorAll('.price__wrapper-list-item')];
 const formInputs = [...document.querySelectorAll('.contact__form > *[data-name="form-element"]')];
@@ -26,6 +25,7 @@ const tabletLandWidth = 1024;
 const desktopLandWidth = 1200;
 
 hamburgerElement.addEventListener('click', () => showHamburger(hamburgerElement));
+form.addEventListener('submit', e => formSubmit(e, formInputs));
 
 priceItems.forEach(item => {
   item.addEventListener('click', () => {
@@ -33,7 +33,6 @@ priceItems.forEach(item => {
   });
 });
 
-//Form remove placeholder on focus
 formInputs.forEach(input => {
   input.addEventListener('focus', function(e) {
     const valueBefore = e.target.placeholder;
@@ -43,27 +42,6 @@ formInputs.forEach(input => {
     });
   });
 });
-
-form.addEventListener('submit', e => formSubmit(e, formInputs));
-
-//Change video when resolution is X
-if (screenWidth >= tabletLandWidth) {
-  navigation.classList.remove('navigation--phone');
-  navigation.classList.add('navigation--large');
-  hamburgerElement.style.display = 'none';
-
-  if (screenWidth >= tabletLandWidth && screenWidth < desktopLandWidth) {
-    changeHero('tablet');
-  }
-
-  if (screenWidth >= desktopLandWidth) {
-    changeHero('desktop');
-  }
-}
-
-//jump.js
-
-console.log(links);
 
 links.forEach(link =>
   link.addEventListener('click', function(e) {
@@ -77,3 +55,17 @@ links.forEach(link =>
     }
   }),
 );
+
+if (screenWidth >= tabletLandWidth) {
+  navigation.classList.remove('navigation--phone');
+  navigation.classList.add('navigation--large');
+  hamburgerElement.style.display = 'none';
+
+  if (screenWidth >= tabletLandWidth && screenWidth < desktopLandWidth) {
+    changeHero('tablet');
+  }
+
+  if (screenWidth >= desktopLandWidth) {
+    changeHero('desktop');
+  }
+}
